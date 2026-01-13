d62fa76c3a51912791c1a67f6695ecbd50f3b58d
Ry <ryan.hixon@gmail.com>
2026-01-13 15:59:19 -0500

    moving files around and centralizing folder location

--- Program.cs
+++ Program.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath("/home/ry/Documents/Projects/OrderProceeing")  //path needs to be set to something else, maybe env var?
+    .AddJsonFile("appsettings.json");
+    
+var configuration = builder.Build();
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("App Started: " + DateTime.Now.ToLocalTime());
+//Console.WriteLine("App Started: " + DateTime.Now.ToLocalTime());
 
-var rootDIR = @"../../../Files/Open";
-var closedFILE = "/closed.dat";
-var waitingForPaymentFILE = "/payment.dat";
+var logFilePath = @"../../../Log_" + 
+    DateTime.UtcNow.Month.ToString("##") + DateTime.UtcNow.Day.ToString("##") + DateTime.UtcNow.Year.ToString("##");
+
+var rootDIR = @"../../../Files";
+var closedFILE = configuration.GetSection("Closed_File").Value;
+var waitingForPaymentFILE = configuration.GetSection("WaitingForPayment_File").Value;
 
 List<String> companyList = new List<string>(Directory.EnumerateDirectories(rootDIR));
 
 List<string> directoriesToClose = new List<string>();
 List<string> directoriesWaitingForPayment = new List<string>();
+
+//create log file
+if (!File.Exists(logFilePath))
+{
+    using (StreamWriter sw = File.CreateText(logFilePath))
+    {
+        sw.WriteLine("Log Started: " + DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToShortTimeString());        
+    }
+}
+
 foreach(var company in companyList)
 {
     List<string> openCases = new List<string>(Directory.EnumerateDirectories(company+"/Open"));
@@ -33,24 +54,71 @@ foreach(var company in companyList)
 
 if(directoriesToClose.Count() > 0)
 {
-    Console.WriteLine("\n################################");
-    Console.WriteLine("Directories that should be closed:");
-    foreach(var item in directoriesToClose)
+    using (StreamWriter sw = File.AppendText(logFilePath))
     {
-        Console.WriteLine(item);
-    }    
-    Console.WriteLine("################################\n");
+        //Console.WriteLine("\n################################");
+        //Console.WriteLine("Directories that should be closed:");
+        sw.WriteLine("\n################################");
+        sw.WriteLine("Directories that should be closed:");
+        foreach(var item in directoriesToClose)
+        {
+            //Console.WriteLine(item);
+            sw.WriteLine(item);
+        } 
+        sw.WriteLine("################################\n");
+        //Console.WriteLine("################################\n");
+    }
 }
 
 if(directoriesWaitingForPayment.Count() > 0)
 {
-    Console.WriteLine("\n################################");
-    Console.WriteLine("Directories that should be placed in hold for payment:");
-    foreach(var item in directoriesWaitingForPayment)
+    using(StreamWriter sw = File.AppendText(logFilePath))
     {
-        Console.WriteLine(item);
+        //Console.WriteLine("\n################################");
+        //Console.WriteLine("Directories that should be placed in hold for payment:");
+        
+        sw.WriteLine("\n################################");
+        sw.WriteLine("Directories that should be placed in hold for payment:");
+        foreach(var item in directoriesWaitingForPayment)
+        {
+            //Console.WriteLine(item);
+            sw.WriteLine(item);
+        }
+        sw.WriteLine("################################\n");
+        //Console.WriteLine("################################\n");   
     }
-    Console.WriteLine("################################\n");
 }
 
-Console.WriteLine("App Completed: " + DateTime.UtcNow.ToLocalTime());
+//Section to move files if in processing or complete
+//move full directory into ../../Closed
+foreach(var closed in directoriesToClose)
+{
+    using (StreamWriter sw = File.AppendText(logFilePath))
+    {
+        sw.WriteLine("\n################################");
+        sw.WriteLine("Directories");
+        //Console.WriteLine("\n################################");
+        //Console.WriteLine("Directories");
+        string destinationPath = closed.Replace("Open", "Closed");
+        if (Path.Exists(destinationPath))
+        {
+            //This will throw an error if the thing already exists
+            //what's our workaround? just copy all files out to the directory that are missing currently
+            Directory.Move(closed, closed.Replace("Open", "Closed"));    
+        }
+        else
+        {
+            //need to create the destination path
+            Directory.Move(closed, closed.Replace("Open", "Closed"));   
+            sw.WriteLine("Moved " + closed + " to " + closed.Replace("Open", "Closed"));
+        }
+
+        sw.WriteLine("################################\n");
+    }
+}
+
+using(StreamWriter sw = File.AppendText(logFilePath))
+{
+    //Console.WriteLine("App Completed: " + DateTime.UtcNow.ToLocalTime());
+    sw.WriteLine("App Completed: " + DateTime.Now.ToLocalTime());
+}
